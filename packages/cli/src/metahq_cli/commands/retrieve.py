c9ae2f4337b7051db9f49b0acbbf2fbdbce526c6
phicks22 <we.can.phicks.it@gmail.com>
2025-09-26 15:20:44 -0600

    added a retriever to standardize processes

--- packages/cli/src/metahq_cli/commands/retrieve.py
+++ packages/cli/src/metahq_cli/commands/retrieve.py
@@ -4,44 +4,84 @@ CLI command to retrieve annotations and labels from meta-hq.
 Author: Parker Hicks
 Date: 2025-09-05
 
-Last updated: 2025-09-23 by Parker Hicks
+Last updated: 2025-09-26 by Parker Hicks
 """
-
-import sys
 
 import click
 import polars as pl
-from metahq_core.ontology.base import Ontology
-from metahq_core.query import Query
 from metahq_core.util.supported import get_onto_families, ontologies
 
+from metahq_cli.retriever import CurationConfig, OutputConfig, QueryConfig, Retriever
 from metahq_cli.util.checkers import check_filters
 from metahq_cli.util.helpers import FilterParser
+from metahq_cli.util.messages import warning
 from metahq_cli.util.supported import REQUIRED_FILTERS
 
 
-def parse_terms(terms: str, reference: str):
-    onto = Ontology.from_obo(ontologies(reference), reference)
+# ===================================================
+# ==== helpers to build retrieval configurations
+# ===================================================
+def parse_onto_terms(terms: str, reference: str):
     available = (
         pl.scan_parquet(get_onto_families(reference)["relations"])
         .collect_schema()
         .names()
     )
 
+    parse = lambda terms: [term for term in terms if term in available]
+
     if terms == "all":
-        return [term for term in list(onto.class_dict.keys()) if term in available]
+        from metahq_core.ontology.base import Ontology
 
-    return [term for term in terms.split(",") if term in available]
+        onto = Ontology.from_obo(ontologies(reference), reference)
+        return parse(list(onto.class_dict.keys()))
 
-
-def warning(message):
-    click.secho(f"WARNING: {message}", fg="yellow")
-
-
-def error(message):
-    click.secho(f"ERROR: {message}", fg="red")
+    return parse(terms.split(","))
 
 
+def make_query_config(db: str, attribute: str, level: str, filters: dict[str, str]):
+    """Construct a query configuration."""
+    return QueryConfig(
+        database=db,
+        attribute=attribute,
+        level=level,
+        ecode=filters["ecode"],
+        species=filters["species"],
+        technology=filters["technology"],
+    )
+
+
+def make_curation_config(terms: str, mode: str, ontology: str):
+    """Construct a curation configuration."""
+    if ontology == "sex":
+        if mode != "direct":
+            warning("Sex queries must be direct annotations.")
+            print("Changing mode to direct...")
+        _terms = terms.split(",")
+
+    else:
+        _terms = parse_onto_terms(terms, ontology)
+
+    return CurationConfig(mode, _terms, ontology)
+
+
+def report_bad_filters(filters):
+    """Check filters and return improper filter parameters."""
+    bad_filters = check_filters(filters)
+    if len(bad_filters) > 0:
+        exc = click.ClickException("Unsupported filter argument")
+        exc.add_note(f"Expected filters in {REQUIRED_FILTERS}, got {bad_filters}.")
+
+
+def set_verbosity(quiet: bool):
+    if quiet:
+        return False
+    return True
+
+
+# ===================================================
+# ==== entry point
+# ===================================================
 @click.group
 def retrieve_commands():
     """Retrieval commands for tissue, disease, sex, and age annotations."""
@@ -58,56 +98,24 @@ def retrieve_commands():
 )
 @click.option("--output", type=click.Path(), default="annotations.parquet")
 @click.option("--fmt", type=str, default="parquet")
-@click.option("--metadata", type=str)
-def retrieve_tissues(terms, level, mode, fmt, metadata, filters, output):
+@click.option("--metadata", type=str, default="sample")
+@click.option("--quiet", is_flag=True)
+def retrieve_tissues(terms, level, mode, fmt, metadata, filters, output, quiet):
     """Retrieval command for tissue ontology terms."""
-    ontology = "uberon"
-    terms = parse_terms(terms, ontology)
+    verbose = set_verbosity(quiet)
+
+    # parse and check filters
     filters = FilterParser.from_str(filters).filters
+    report_bad_filters(filters)
 
-    bad_filters = check_filters(filters)
-    if len(bad_filters) > 0:
-        exc = click.ClickException("Unsupported filter argument")
-        exc.add_note(f"Expected filters in {REQUIRED_FILTERS}, got {bad_filters}.")
+    # make configs
+    query_config = make_query_config("geo", "tissue", level, filters)
+    curation_config = make_curation_config(terms, mode, "uberon")
+    output_config = OutputConfig(output, fmt, metadata)
 
-    curation = Query(
-        database="geo",
-        attribute="tissue",
-        level=level,
-        ecode=filters["ecode"],
-        species=filters["species"],
-        technology=filters["technology"],
-    ).annotations()
-
-    if mode == "direct":
-        terms_with_anno = [term for term in terms if term in curation.entities]
-        not_in_anno = [term for term in terms if not term in terms_with_anno]
-        if len(not_in_anno) == len(terms):
-            error(
-                "No direct annotations for any terms. Try propagating or different contitions."
-            )
-            click.echo("Exiting...")
-            sys.exit(1)
-
-        if len(terms_with_anno) != len(terms):
-            warning(
-                f"Warning: {not_in_anno} have no direct annotations. Try propagating or different conditions."
-            )
-
-        curation = curation.select(terms_with_anno).filter(
-            pl.any_horizontal(pl.col(terms_with_anno) == 1)
-        )
-    elif mode == "propagate":
-        curation = curation.propagate(terms, ontology, mode=0)
-
-    elif mode == "label":
-        curation = curation.propagate(terms, ontology, mode=1)
-
-    else:
-        exc = click.ClickException("Unsupported mode argument")
-        exc.add_note(f"Expected mode in [direct, propagated, labels], got {mode}")
-
-    curation.save(output, fmt, metadata)
+    # retrieve
+    retriever = Retriever(query_config, curation_config, output_config, verbose)
+    retriever.retrieve()
 
 
 @retrieve_commands.command("diseases")
@@ -120,72 +128,36 @@ def retrieve_tissues(terms, level, mode, fmt, metadata, filters, output):
     type=str,
     default="species=human,ecode=expert-curated,technology=rnaseq",
 )
-@click.option("--metadata", type=str)
+@click.option("--metadata", type=str, default="sample")
 @click.option("--output", type=click.Path(), default="annotations.parquet")
-def retrieve_diseases(terms, level, mode, fmt, metadata, filters, output):
+@click.option("--quiet", is_flag=True, default=False)
+def retrieve_diseases(terms, level, mode, fmt, metadata, filters, output, quiet):
     """Retrieval command for disease ontology terms."""
-    ontology = "mondo"
-    terms = parse_terms(terms, ontology)
+    verbose = set_verbosity(quiet)
+
+    # parse and check filters
     filters = FilterParser.from_str(filters).filters
+    report_bad_filters(filters)
 
-    bad_filters = check_filters(filters)
-    if len(bad_filters) > 0:
-        exc = click.ClickException("Unsupported filter argument")
-        exc.add_note(f"Expected filters in {REQUIRED_FILTERS}, got {bad_filters}.")
+    # make configs
+    query_config = make_query_config("geo", "disease", level, filters)
+    curation_config = make_curation_config(terms, mode, "mondo")
+    output_config = OutputConfig(output, fmt, metadata)
 
-    curation = Query(
-        database="geo",
-        attribute="disease",
-        level=level,
-        ecode=filters["ecode"],
-        species=filters["species"],
-        technology=filters["technology"],
-    ).annotations()
-
-    if mode == "direct":
-        terms_with_anno = [term for term in terms if term in curation.entities]
-        not_in_anno = [term for term in terms if not term in terms_with_anno]
-        if len(not_in_anno) == len(terms):
-            error(
-                "No direct annotations for any terms. Try propagating or different contitions."
-            )
-            click.echo("Exiting...")
-            sys.exit(1)
-
-        if len(terms_with_anno) != len(terms):
-            warning(
-                f"Warning: {not_in_anno} have no direct annotations. Try propagating or different conditions."
-            )
-
-        curation = curation.select(terms_with_anno).filter(
-            pl.any_horizontal(pl.col(terms_with_anno) == 1)
-        )
-    elif mode == "propagate":
-        curation = curation.propagate(terms, ontology, mode=0)
-
-    elif mode == "label":
-        curation = curation.propagate(terms, ontology, mode=1)
-
-    else:
-        exc = click.ClickException("Unsupported mode argument")
-        exc.add_note(f"Expected mode in [direct, propagated, labels], got {mode}")
-
-    curation.save(output, fmt, metadata)
+    # retrieve
+    retriever = Retriever(query_config, curation_config, output_config, verbose)
+    retriever.retrieve()
 
 
 @retrieve_commands.command("sex")
 @click.option("--terms", type=str, default="male,female")
-@click.option(
-    "--filters", type=str, default="species=human,db=geo,ecode=expert-curated"
-)
+@click.option("--filters", type=str, default="species=human,ecode=expert-curated")
 def retrieve_sex(terms, fmt, include_metadata, filters, output):
     pass
 
 
 @retrieve_commands.command("age")
 @click.option("--terms", type=str, default="10-20,70-80")
-@click.option(
-    "--filters", type=str, default="species=human,db=geo,ecode=expert-curated"
-)
+@click.option("--filters", type=str, default="species=human,ecode=expert-curated")
 def retrieve_age(terms, fmt, include_metadata, filters, output):
     pass
