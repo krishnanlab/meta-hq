608e42afd79b89d1769ebab536fe245fc8555ced
Parker Hicks <we.can.phicks.it@gmail.com>
2025-11-21 13:48:02 -0700

    fixed default initialization

--- packages/cli/src/metahq_cli/setup/config.py
+++ packages/cli/src/metahq_cli/setup/config.py
@@ -4,22 +4,26 @@ Command to setup the metahq configuration.
 Author: Parker Hicks
 Date: 2025-09-05
 
-Last updated: 2025-10-13
+Last updated: 2025-11-21 by Parker Hicks
 """
 
 import sys
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import yaml
+from metahq_core.util.progress import console
+from metahq_core.util.supported import get_config_file_no_check
 
-from metahq_core.util.io import checkdir
+from metahq_cli.logger import setup_logger
 
-HOME_DIR = Path.home()
-METAHQ_DIR = checkdir(Path.home() / "metahq")
-CONFIG = METAHQ_DIR / "config.yaml"
+if TYPE_CHECKING:
+    import logging
 
 
-# TODO: Remove test func
+CONFIG_FILE: Path = get_config_file_no_check()
+
+
 class Config:
     """
     Class to store and assess and create the meta-hq configuration file.
@@ -28,38 +32,59 @@ class Config:
 
     def __init__(
         self,
-        version="0.1.0",
-        zenodo_doi="xxxx1",
-        data_dir=str(HOME_DIR / "data"),
-        logs=str(HOME_DIR / ".logs" / "metahq.log"),
+        version,
+        zenodo_doi,
+        data_dir,
+        logs,
+        logger=None,
+        loglevel=20,
+        verbose=True,
     ):
         self.version: str = version
         self.zenodo_doi: str = zenodo_doi
         self.data_dir: str = data_dir
         self.logs: str = logs
 
+        if logger is None:
+            logger = setup_logger(
+                __name__, level=loglevel, log_dir=logs, console=console
+            )
+        self.logger: logging.Logger = logger
+
+        self.verbose = verbose
+
         self.ok_keys: list[str] = ["version", "zenodo_doi", "data_dir", "logs"]
 
     def check(self):
         """Checks if the meta-hq config exists. Initializes if not."""
-        if not CONFIG.exists():
-            self.set_default()
+        if not CONFIG_FILE.exists():
+            if self.verbose:
+                self.logger.debug("Initializing new config file %s", CONFIG_FILE)
+            CONFIG_FILE.touch()
 
         else:
+            if self.verbose:
+                self.logger.debug("Config file exists: %s", CONFIG_FILE)
+                self.logger.debug("Running config check...")
             # check existing
             if not self.is_acceptable_config():
-                print("Incorrect configuration detected. Resetting with defaults.")
+                if self.verbose:
+                    self.logger.warning(
+                        "Incorrect configuration detected. Resetting with defaults."
+                    )
                 self.set_default()
 
     def is_acceptable_config(self):
         """Checks if config has correct structure."""
         config = self.load_config()
 
+        self.logger.debug("Existing config: %s", config)
+
         return sorted(list(config.keys())) == sorted(self.ok_keys)
 
     def load_config(self) -> dict[str, str]:
         """Loads the meta-hq config file."""
-        with open(CONFIG, "r", encoding="utf-8") as stream:
+        with open(CONFIG_FILE, "r", encoding="utf-8") as stream:
             try:
                 return yaml.safe_load(stream)
             except yaml.YAMLError as e:
@@ -67,7 +92,7 @@ class Config:
 
     def load_config_str(self) -> str:
         """Loads the meta-hq config file."""
-        with open(CONFIG, "r", encoding="utf-8") as f:
+        with open(CONFIG_FILE, "r", encoding="utf-8") as f:
             return f.read()
 
     def make_config(self) -> dict[str, str]:
@@ -81,43 +106,40 @@ class Config:
 
     def save_config(self, config: dict[str, str]):
         """Saves a config file."""
-        with open(CONFIG, "w", encoding="utf-8") as stream:
+        self.logger.info("Saving MetaHQ config to %s", CONFIG_FILE)
+        with open(CONFIG_FILE, "w", encoding="utf-8") as stream:
             try:
                 yaml.safe_dump(config, stream)
             except yaml.YAMLError as e:
                 sys.exit(str(e))
+        self.logger.info("Done!")
 
     def setup(self):
         """Main setup function."""
         self.check()
-        new = self.update_config()
+        new = self.initialize_config()
         self.save_config(new)
 
     def set_default(self):
         """Makes a default meta-hq config."""
-        print("making default")
+        self.logger.info("Making config with default arguments.")
         self.save_config(self.make_config())
 
-    def update_config(self):
+    def initialize_config(self):
         """Updated the meta-hq config file."""
-        config = self.load_config()
-
-        if self.data_dir == "default":
-            self.data_dir = str(Path.home() / "metahq" / "data")
-
-        new_vars = {
+        config = {
             "version": self.version,
             "zenodo_doi": self.zenodo_doi,
             "data_dir": str(Path(self.data_dir).resolve()),
+            "logs": self.logs,
         }
 
-        config = {
-            key: new_vars[key] if key in new_vars else config[key] for key in config
-        }
+        if self.verbose:
+            self.logger.debug("Initialized configuration: %s", config)
 
         return config
 
     @property
     def path(self) -> str:
         """Returns /path/to/config.yaml"""
-        return str(CONFIG)
+        return str(CONFIG_FILE)
