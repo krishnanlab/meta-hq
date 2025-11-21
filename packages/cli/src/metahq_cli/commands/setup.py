2845b87ea5b2f4629df286d2260599469910ca76
Parker Hicks <we.can.phicks.it@gmail.com>
2025-11-21 13:46:46 -0700

    added database download functionality

--- packages/cli/src/metahq_cli/commands/setup.py
+++ packages/cli/src/metahq_cli/commands/setup.py
@@ -4,23 +4,58 @@ Command to set up the meta-hq CLI.
 Author: Parker Hicks
 Date: 2025-09-05
 
-Last updated: 2025-09-05
+Last updated: 2025-11-21 by Parker Hicks
 """
 
 from importlib.metadata import version
 
 import click
-from metahq_core.setup import Config
+from metahq_core.util.progress import console
+from metahq_core.util.supported import get_default_data_dir, get_default_log_dir
+
+from metahq_cli.logger import setup_logger
+from metahq_cli.setup.config import Config
+from metahq_cli.setup.downloader import Downloader
+from metahq_cli.util.helpers import set_verbosity
+from metahq_cli.util.supported import LATEST_DATABASE, log_level_opt
 
 METAHQ_VERSION = version("metahq_core")
 
 
 @click.command
-@click.option("--zenodo_doi", type=str, default="latest")
+@click.option("--doi", type=str, default="latest")
 @click.option("--data_dir", type=click.Path(), default="default")
-def setup(zenodo_doi: str, data_dir: str):
+@click.option("--loglevel", type=log_level_opt(), default="info", help="Logging level.")
+@click.option(
+    "--logdir",
+    type=str,
+    default="default",
+    help="Path to directory storing logs.",
+)
+@click.option(
+    "--quiet", is_flag=True, default=False, help="No log or console output if applied."
+)
+def setup(doi: str, data_dir: str, loglevel: str, logdir: str, quiet: bool):
     """Creates the meta-hq package configuration file."""
-    config = Config(METAHQ_VERSION, zenodo_doi, data_dir)
-    config.check()
+    if logdir == "default":
+        logdir = str(get_default_log_dir())
+
+    logger = setup_logger(__name__, level=loglevel, log_dir=logdir, console=console)
+    verbose = set_verbosity(quiet)
+
+    if doi == "latest":
+        doi = LATEST_DATABASE["doi"]
+
+    if data_dir == "default":
+        data_dir = str(get_default_data_dir())
+
+    logger.info("Downloading MetaHQ database...")
+    downloader = Downloader(doi, data_dir, logger=logger, verbose=verbose)
+    downloader.get()
+    downloader.extract()
+
+    logger.info("Configuring MetaHQ...")
+    config = Config(
+        METAHQ_VERSION, doi, data_dir, logdir, logger=logger, verbose=verbose
+    )
     config.setup()
-    click.echo(f"Config saved to {config.path}.")
