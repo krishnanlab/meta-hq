7e31fc059695196e9692ff357dbe8893c191d470
phicks22 <we.can.phicks.it@gmail.com>
2025-11-24 11:46:26 -0700

    added logger_args from common_args

--- packages/cli/src/metahq_cli/commands/setup.py
+++ packages/cli/src/metahq_cli/commands/setup.py
@@ -18,24 +18,20 @@ from metahq_core.util.supported import (
 from metahq_cli.logger import setup_logger
 from metahq_cli.setup.config import Config
 from metahq_cli.setup.downloader import Downloader
+from metahq_cli.util.common_args import logging_args
 from metahq_cli.util.helpers import set_verbosity
 from metahq_cli.util.supported import LATEST_DATABASE
 
-LOG_LEVEL_OPT = click.Choice(supported("log_levels"))
-
 
 @click.command
+@logging_args
 @click.option("--doi", type=str, default="latest")
 @click.option("--data_dir", type=click.Path(), default="default")
-@click.option("--loglevel", type=LOG_LEVEL_OPT, default="info", help="Logging level.")
 @click.option(
     "--logdir",
     type=str,
     default="default",
     help="Path to directory storing logs.",
-)
-@click.option(
-    "--quiet", is_flag=True, default=False, help="No log or console output if applied."
 )
 def setup(doi: str, data_dir: str, loglevel: str, logdir: str, quiet: bool):
     """Creates the meta-hq package configuration file."""
