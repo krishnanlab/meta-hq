3748133eddac0f20f381e9b9e42fc15d37820922
phicks22 <we.can.phicks.it@gmail.com>
2025-11-26 09:58:43 -0700

    improved docstrings

--- packages/cli/src/metahq_cli/commands/setup.py
+++ packages/cli/src/metahq_cli/commands/setup.py
@@ -4,15 +4,14 @@ Command to set up the meta-hq CLI.
 Author: Parker Hicks
 Date: 2025-09-05
 
-Last updated: 2025-11-24 by Parker Hicks
+Last updated: 2025-11-26 by Parker Hicks
 """
+
+from pathlib import Path
 
 import click
 from metahq_core.util.progress import console
-from metahq_core.util.supported import (
-    get_default_data_dir,
-    get_default_log_dir,
-)
+from metahq_core.util.supported import get_default_data_dir, get_default_log_dir
 
 from metahq_cli.logger import setup_logger
 from metahq_cli.setup.config import Config
@@ -23,17 +22,27 @@ from metahq_cli.util.supported import LATEST_DATABASE
 
 
 @click.command
-@logging_args
-@click.option("--doi", type=str, default="latest")
-@click.option("--data-dir", type=click.Path(), default="default")
+@click.option(
+    "--doi",
+    type=str,
+    default="latest",
+    help="Zenodo DOI of the MetaHQ database. Default is `latest`.",
+)
+@click.option(
+    "--data-dir",
+    type=click.Path(),
+    default="default",
+    help="Path to directory to store the database. Default is `/home/path/.metahq_data`.",
+)
 @click.option(
     "--log-dir",
-    type=str,
+    type=click.Path(),
     default="default",
-    help="Path to directory storing logs.",
+    help="Path to directory storing logs. Default is `/home/path/MetaHQ`.",
 )
+@logging_args
 def setup(doi: str, data_dir: str, log_level: str, log_dir: str, quiet: bool):
-    """Creates the meta-hq package configuration file."""
+    """Downloads the MetahQ database and configures the CLI."""
     if log_dir == "default":
         log_dir = str(get_default_log_dir())
 
@@ -48,7 +57,7 @@ def setup(doi: str, data_dir: str, log_level: str, log_dir: str, quiet: bool):
 
     logger.info("Downloading MetaHQ database...")
     downloader = Downloader(
-        doi, data_dir, logger=logger, logdir=log_dir, verbose=verbose
+        doi, data_dir, logger=logger, logdir=str(log_dir), verbose=verbose
     )
     downloader.get()
     downloader.extract()
