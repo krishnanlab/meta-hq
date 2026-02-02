87f65edb42e74134a788ca00ecec0193440ca2d2
ChristopherMancuso <chrismancuso1984@gmail.com>
2026-02-01 21:26:27 -0500

    added functionailty for deleting

--- packages/cli/src/metahq_cli/commands/__init__.py
+++ packages/cli/src/metahq_cli/commands/__init__.py
@@ -1,7 +1,8 @@
+from metahq_cli.commands.delete import delete
 from metahq_cli.commands.retrieve import retrieve_commands
 from metahq_cli.commands.search import search
 from metahq_cli.commands.setup import setup
 from metahq_cli.commands.supported import supported
 from metahq_cli.commands.validate import validate
 
-__all__ = ["retrieve_commands", "setup", "search", "supported", "validate"]
+__all__ = ["retrieve_commands", "setup", "search", "supported", "validate", "delete"]
--- packages/cli/src/metahq_cli/main.py
+++ packages/cli/src/metahq_cli/main.py
@@ -9,7 +9,7 @@ Last updated: 2025-09-05
 
 import click
 
-from metahq_cli.commands import retrieve_commands, search, setup, supported, validate
+from metahq_cli.commands import retrieve_commands, search, setup, supported, validate, delete
 
 
 @click.group()
@@ -22,6 +22,7 @@ main.add_command(search)
 main.add_command(supported)
 main.add_command(retrieve_commands, name="retrieve")
 main.add_command(validate)
+main.add_command(delete)
 
 if __name__ == "__main__":
     main()
