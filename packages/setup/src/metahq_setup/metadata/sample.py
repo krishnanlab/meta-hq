a9fe9408b17d2b78517e6e7fd528ee0a1dbcd069
phicks22 <we.can.phicks.it@gmail.com>
2026-04-30 12:55:22 -0600

    added metadata-based constants as default values in SampleMetadataRetriever

--- packages/setup/src/metahq_setup/metadata/sample.py
+++ packages/setup/src/metahq_setup/metadata/sample.py
@@ -7,71 +7,27 @@ from pathlib import Path
 import duckdb
 import polars as pl
 
-from metahq_setup.config.config import OMICIDX_DB
+from metahq_setup.config.config import (
+    OMICIDX_COL_ACCESSION,
+    OMICIDX_DB,
+    OMICIDX_SAMPLE_TABLE,
+)
 from metahq_setup.metadata.base import BaseMetadataRetriever
 
 
 class SampleMetadataRetriever(BaseMetadataRetriever):
     """Perform sample-level queries to collect metadata from OmicIDX."""
 
-    def __init__(self, db_path: Path = OMICIDX_DB, table: str = "src_geo_samples"):
-        super().__init__()
-        self.db_path = db_path
-        self.table = table
-
-    def get_available_fields(
-        self,
-        channel_name: str = "channels",
-    ) -> tuple[list[str], list[str]]:
-        """Identify the available metadata fields to query.
-
-        Arguments:
-            db_path (Path):
-                Path to the OmicIDX database.
-            table (str):
-                Name of the table in OmicIDX to query.
-            channel_name (str):
-                Name of the channels column.
-        Returns:
-            (tuple[list[str], list[str]]): All fields available to query and
-                channel specific fields are those are hidden in a struct in
-                the channel_name column.
-        """
-        with duckdb.connect(self.db_path, read_only=True) as conn:
-            result = conn.execute(
-                """
-                SELECT table_name, column_name
-                FROM information_schema.columns
-                WHERE table_name = $1;
-                """,
-                [self.table],
-            ).fetchall()
-
-            fields = [row[1] for row in result]
-            if channel_name in fields:
-                result = conn.execute(
-                    f"SELECT unnest({channel_name}) FROM {self.table} LIMIT 1;",
-                ).fetchall()
-
-                channel_fields = list(result[0][0].keys())
-                fields.extend(channel_fields)
-            else:
-                channel_fields = []
-
-            return fields, channel_fields
-
-    def get_available_tables(self) -> list[str]:
-        """Show available tables in OmicIDX."""
-        with duckdb.connect(self.db_path, read_only=True) as conn:
-            result = conn.execute(
-                "SELECT table_name from information_schema.tables"
-            ).fetchall()
-
-        return [i[0] for i in result]
+    def __init__(self, db_path: Path = OMICIDX_DB, table: str = OMICIDX_SAMPLE_TABLE):
+        super().__init__(db_path=db_path, table=table)
 
     def retrieve(
-        self, fields: list[str], samples: list[str], accession_name: str = "accession"
+        self,
+        fields: list[str],
+        samples: list[str],
+        accession_name: str = OMICIDX_COL_ACCESSION,
     ):
+        """Retrieve sample metadata from OmicIDX."""
         if accession_name not in fields:
             fields.append(accession_name)
 
@@ -81,8 +37,8 @@ class SampleMetadataRetriever(BaseMetadataRetriever):
         self.logger.info("Checking queried fields...")
         fields = self._check_fields(fields, available_fields)
 
+        # if necessary, add workaround for channel fields because they are nested
         queried_channel_fields = [field for field in fields if field in channel_fields]
-
         if len(queried_channel_fields) > 0:
             self.logger.info(
                 "Found channel fields to query: %s", queried_channel_fields
@@ -107,14 +63,6 @@ class SampleMetadataRetriever(BaseMetadataRetriever):
 
         self.metadata = pl.DataFrame(result_dict)
 
-    def show_available_fields(self):
-        all_fields, _ = self.get_available_fields()
-        self.logger.info("Available fields: %s", all_fields)
-
-    def _build_base_query(self, query_fields: list[str], accession_name: str) -> str:
-        formatted_fields = ", ".join(query_fields)
-        return f"""SELECT {formatted_fields} FROM {self.table} WHERE {accession_name} = ANY($1)"""
-
     def _build_channels_query(
         self, fields: list[str], channel_fields: list[str], accession_name: str
     ) -> str:
@@ -130,14 +78,3 @@ class SampleMetadataRetriever(BaseMetadataRetriever):
         self.logger.info("Querying fields: %s", ", ".join(fields))
 
         return f"""SELECT {formatted_fields} FROM {self.table} WHERE {accession_name} = ANY($1)"""
-
-    def _check_fields(self, queried_fields: list[str], available_fields: list[str]):
-        ok_fields = []
-        for field in queried_fields:
-            if field not in available_fields:
-                self.logger.warning("%s not in available fields. Skipping...", field)
-                continue
-
-            ok_fields.append(field)
-
-        return ok_fields
