8d7d401c4cc6d0dd2956d4adc92befbb14bc5d8e
phicks22 <we.can.phicks.it@gmail.com>
2026-05-12 15:31:40 -0600

    moved AGE_GROUPS constant to config module

--- packages/db_build/src/metahq_build/util/age_groups.py
+++ packages/db_build/src/metahq_build/util/age_groups.py
@@ -2,20 +2,9 @@
 Age group classification utilities.
 
 Maps a continuous age in years to a discrete age group label.
-
-Author: Parker Hicks
-Date: 2025-10-14
 """
 
-AGE_GROUPS = [
-    {"name": "fetus", "min_age": -1, "max_age": 0},
-    {"name": "infant", "min_age": 0, "max_age": 2},
-    {"name": "child", "min_age": 2, "max_age": 10},
-    {"name": "adolescent", "min_age": 10, "max_age": 20},
-    {"name": "adult", "min_age": 20, "max_age": 50},
-    {"name": "older_adult", "min_age": 50, "max_age": 80},
-    {"name": "elderly_adult", "min_age": 80, "max_age": 150},
-]
+from metahq_build.config import AGE_GROUPS
 
 
 def get_age_group(age: float) -> str | None:
