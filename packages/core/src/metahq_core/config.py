eeed7b40814b7a18d3824dc0a8e67443a1123c3b
phicks22 <we.can.phicks.it@gmail.com>
2026-08-14 12:58:16 -0600

    feat(config): adds sample and series key constants

--- packages/core/src/metahq_core/config.py
+++ packages/core/src/metahq_core/config.py
@@ -10,3 +10,6 @@ Last updated: 2026-04-13 by Parker Hicks
 # name of the column containing sources in a query result
 SOURCES_COL = "sources"
 EXTERNAL_LINKS_COL = "external_links"
+
+SAMPLE_KEY = "sample"
+SERIES_KEY = "series"
