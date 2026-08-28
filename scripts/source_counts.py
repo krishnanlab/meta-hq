28029f3992234c4d89743fac43a971c97d8e9386
phicks22 <we.can.phicks.it@gmail.com>
2026-08-28 16:33:26 -0600

    refactor(scripts): adds argument parsing to source counts script

--- scripts/source_counts.py
+++ scripts/source_counts.py
@@ -7,21 +7,32 @@ Date: 2026-04-01
 Last updated: 2026-04-01 by Parker Hicks
 """
 
+from argparse import ArgumentParser
+from pathlib import Path
 from pprint import pprint
 
 from metahq_core.sources import REFERENCE_MAP
 from metahq_core.util.io import load_bson
-from metahq_core.util.supported import get_annotations
 
 
-def get_total_annotation_count():
+def total(counts: dict[str, dict[str, int]]) -> dict[str, int]:
+    """Sum all values in the counts dict"""
+    sums = {"sample": 0, "series": 0}
+    for level in sums:
+        sources = counts[level].keys()
+        for source in sources:
+            sums[level] += counts[level][source]
+    return sums
+
+
+def get_total_annotation_count(dbs):
     """
     Count the total number of annotations for each annotation source across attributes.
     Sources can provide more than one annotation type for a particular sample or study.
     """
     counts = {"sample": {}, "series": {}}
     for level in counts:
-        data = load_bson(get_annotations(level))
+        data = load_bson(dbs[level])
 
         for entry in data.values():
             for attribute in ["tissue", "disease", "sex", "age"]:
@@ -36,7 +47,7 @@ def get_total_annotation_count():
     return counts
 
 
-def get_total_entry_count():
+def get_total_entry_count(dbs):
     """
     Only count the number of samples or studies a source has annotations for,
     not the number of total annotations (a sample can have multiple annotations
@@ -44,7 +55,7 @@ def get_total_entry_count():
     """
     counts = {"sample": {}, "series": {}}
     for level in counts:
-        data = load_bson(get_annotations(level))
+        data = load_bson(dbs[level])
 
         for entry in data.values():
             for source in REFERENCE_MAP:
@@ -65,12 +76,29 @@ def get_total_entry_count():
 
 def main():
     """Count entries per source."""
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--sample-db",
+        help="Path to MetaHQ sample-level BSON database.",
+        type=Path,
+        default="data/processed/combined__level-sample.bson",
+    )
+    parser.add_argument(
+        "--series-db",
+        help="Path to MetaHQ series-level BSON database.",
+        type=Path,
+        default="data/processed/combined__level-series.bson",
+    )
+    args = parser.parse_args()
 
-    all_counts = get_total_annotation_count()
-    entry_counts = get_total_entry_count()
+    dbs = {"sample": args.sample_db, "series": args.series_db}
+
+    all_counts = get_total_annotation_count(dbs)
+    entry_counts = get_total_entry_count(dbs)
 
     print("Total annotation counts:")
     pprint(all_counts)
+    print(f"Total: {total(all_counts)}")
     print("\n")
 
     print("Total entry counts:")
